b44564eb2b10cfca67fd8da9df0b80c3a1e48d07
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-03-11 18:00:01 -0500

    Set controller and action for dashboard/show

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV["ADMIN_USERNAME"], :password => ENV["ADMIN_PASSWORD"]
+
+  # before_filter :authorize
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
